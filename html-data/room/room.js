97221fc47789d8d1f2b6d27b2815a03e1cc3d70a
livles <lucy.yang@outlook.de>
2025-04-04 18:46:23 +0200

    added orbit controls

--- html-data/room/room.js
+++ html-data/room/room.js
@@ -1,13 +1,12 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-
-const camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
-camera.position.z = 13;
-camera.position.set(5, 5, 13);
+import {OrbitControls} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+camera.position.set(5, 3, 13);
 
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x404040);
+// scene.background = new THREE.Color(0x404040);
 const loader = new GLTFLoader();
 let room;
 loader.load('./room.glb',
@@ -18,14 +17,22 @@ loader.load('./room.glb',
     function (error) {}
 );
 
-const renderer = new THREE.WebGLRenderer({alpha:true});
+const ambientLight = new THREE.AmbientLight(0x404040, 3);
+scene.add(ambientLight);
+
+// const spotLight = new THREE.SpotLight(0xffffff, 2, 100, Math.PI / 6, 0.3);
+// spotLight.castShadow = true;
+// spotLight.position.set(5, 10, 6);
+
+// const topLight = new THREE.DirectionalLight(0x404040, 0.5);
+// topLight.position.set(0, 10, 0);
+// topLight.castShadow = true;
+// scene.add(topLight);
+
+const renderer = new THREE.WebGLRenderer({alpha:false, antialias: true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.getElementById('container3D').appendChild(renderer.domElement);
 
-// const geometry = new THREE.TorusGeometry(10,3,16,100);
-// const material =new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
-// const torus = new THREE.Mesh(geometry, material);
-// scene.add(torus);
 
 const animate = () => {
     requestAnimationFrame(animate);
