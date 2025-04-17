d6baa74098fb0430d7297fd54924c4921fc7bb21
livles <lucy.yang@outlook.de>
2025-04-17 12:49:37 +0200

    canvas changes when modal open!!! :)

--- html-data/room/room.js
+++ html-data/room/room.js
@@ -2,12 +2,22 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
 
-
 const canvas = document.getElementById("experience-canvas");
 const renderer = new THREE.WebGLRenderer({canvas: canvas,alpha:false, antialias: true});
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
+let lighton = true;
+const exposureHigh = -3;
+const exposureLow = -5;
+let modalOpen = 1;
 let camera,scene,controls, intersects;
+
+const modals = {
+    about:  document.querySelector(".modal.about"),
+    games:  document.querySelector(".modal.games"),
+    nlp:    document.querySelector(".modal.nlp")
+};
+
 
 function setupScene() {
     
@@ -18,14 +28,14 @@ function setupScene() {
     renderer.toneMappingExposure = Math.pow(2, -3);
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
     
     camera = new THREE.PerspectiveCamera(15,window.innerWidth / window.innerHeight,0.1,1000);
     camera.position.set(-30, 0, 30);
     
     scene = new THREE.Scene();
     scene.castShadow = true;
-    // scene.background = new THREE.Color( 0x404040 );
+    scene.background = new THREE.Color( 0x404040 );
     
     
 }
@@ -34,11 +44,11 @@ function setupControls() {
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.update();  
-
+    
 }
 
 function setupLight() {
-
+    
     const redlight = new THREE.PointLight( 0xff0000,2, 5, 1);
     redlight.position.set( 3, 3, 1 );
     scene.add( redlight );
@@ -60,9 +70,9 @@ function setupLight() {
     const ambientLight = new THREE.AmbientLight(0x404040,2);
     scene.add(ambientLight);
 }
-    
-function load3DModel() {
 
+function load3DModel() {
+    
     const loader = new GLTFLoader();
     loader.load('./room2.glb',
         function(gltf) {
@@ -75,56 +85,125 @@ function load3DModel() {
 
 
 function animate (){
-    // camera.updateMatrixWorld();
     renderer.render ( scene, camera );
     controls.update();
 };
 
 function onPointer( event ) {
     
-    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointer.x = ( event.clientX / canvas.clientWidth ) * 2 - 1;
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     raycaster.setFromCamera( pointer, camera );
     
     if ( scene.children.length > 6 ) {
-
+        
         intersects = raycaster.intersectObjects( scene.children[6].children );
         if ( 0 != intersects.length && intersects[0].object.name.includes("Raycaster")) {
             document.body.style.cursor = "pointer";
         } else {
             document.body.style.cursor = "default";
         }
-
+        
     }
-
+    
 }
 
 
 function onWindowResize () {
+    if (modalOpen > 0) {
+        console.log(modalOpen);
+        canvas.style.width = "50%";
+         camera.aspect = canvas.clientWidth / window.innerHeight ;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth /2 ,window.innerHeight );
     
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    } else {
+        
+        canvas.style.width = "100%";
+        camera.aspect = window.innerWidth/ window.innerHeight ;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth  ,window.innerHeight );
     
+    }
+}
+
+function setupButtons () {
+    
+    // const exit_buttons = 
+    document.querySelectorAll( ".exit-modal" )
+    .forEach( (button) =>
+        // for (let i = 0; i<exit_buttons.length; i++) {
+    // const button = exit_buttons[i];
+    button.addEventListener ( "click" , (e) => {
+        const modal = e.target.closest ( ".modal" );
+        hideModal(modal);
+    })
+    //; }
+    )
+}
+
+function setupTouch () {
+
+    window.addEventListener( "pointermove", 
+       onPointer
+        );
+        window.addEventListener ( "pointerdown", onPointer);
+    
+    window.addEventListener( "pointerup", 
+       onClick
+        );
+
 }
 
 function onClick () {
     if (intersects.length > 0) {
-        intersects[0].object;
+        const name = intersects[0].object.name;
+        console.log(name);
+        if ( name.includes ("schild") ) {
+            showModal(modals.about);
+        } else if ( name.includes ( "Controller" ) ) {
+            showModal(modals.games);
+        } else if ( name.includes ( "NLP" ) ) {
+            showModal (modals.nlp);
+        } else if (name.includes ( "switch" ) ) {
+            if (lighton) {
+                console.log("lighton");
+                renderer.toneMappingExposure = Math.pow(2, exposureLow);
+                scene.background = new THREE.Color( 0 );
+                lighton = false;
+            } else {
+                renderer.toneMappingExposure = Math.pow(2,exposureHigh);
+                scene.background = new THREE.Color( 0x404040 );
+                lighton = true;
+            }
+        }
     }
+}
+
+
+function showModal ( modal ) {
+    modal.style.display = "flex";
+    modalOpen += 1 ;
+    onWindowResize();    
+}; 
+
+function hideModal ( modal ) {
+    modal.style.display = "none";
+    modalOpen -= 1;
+    onWindowResize();
 }
 
 function main () {
     setupScene();
     load3DModel();
     setupLight();
-
+    setupButtons ();
     setupControls();
+    setupTouch ();
     controls.listenToKeyEvents (renderer.domElement);
     
-    window.addEventListener( 'mousemove', onPointer );
     window.addEventListener( 'resize', onWindowResize );
-    window.addEventListener( 'click', onClick );
+    gsap;
 }
 
 main();
